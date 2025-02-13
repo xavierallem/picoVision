f10435611c19d69a0c9981b9e9abb4cf260b1801
Allen Xavier Arasan <60181637+xavierallem@users.noreply.github.com>
2025-02-13 22:28:57 +0000

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,8 @@
 #include "EasyHTTPClient.h"
 
- char* ssid = "www";
- char* password  = "P29hSk73bu";
-  const char* serverName = "http://192.168.178.44:5000";
+ char* ssid = "";
+ char* password  = "";
+  const char* serverName = "";
 
 EasyHTTPClient httpClient(ssid, password, serverName);
 
